117ba44cf1d192cd4975bdb34817c5965bf8d75e
Filin15488 <filya13579@yandex.ru>
2024-02-13 17:52:09 +0300

    preview main page

--- www/index.php
+++ www/index.php
@@ -13,12 +13,38 @@
         <div class="separator"></div>
         <div class="content-container">
             <div class="content-row">
-                <div class="content-column">Работа со стеганопрограммами:</div>
-                <div class="content-column">Стеганографические программы:</div>
+                <div class="content-column">
+                    Работа со стеганопрограммами:
+                    <div class="content-img">
+                        <img src="./images/steganoprograms_work.png" alt="">
+                    </div>
+                </div>
+                <div class="content-column">Стеганографические программы:
+                    <div class="content-img">
+                        <img src="./images/steganoprograms.png" alt="">
+                    </div>
+                    <div class="authentication-button">
+                        <button type="submit" class="btn">
+                            СПИСОК УЧТЕННЫХ 
+                            <br>
+                            СТЕГАНОГРАФИЧЕСКИХ ПРОГРАММ
+                        </button>
+                    </div>
+                </div>
             </div>
             <div class="content-row">
-                <div class="content-column">Идентификация стеганопрограмм:</div>
-                <div class="content-column">Поиск следов присутствия:</div>
+                <div class="content-column">
+                    Идентификация стеганопрограмм:
+                    <div class="content-img">
+                        <img src="./images/steganoprograms_identification.png" alt="">
+                    </div>
+                </div>
+                <div class="content-column">
+                    Поиск следов присутствия:
+                    <div class="content-img">
+                        <img src="./images/steganoprograms_search.png" alt="">
+                    </div>
+                </div>
             </div>
         </div>
             
