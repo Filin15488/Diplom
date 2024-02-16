3b7140110df7e93d6d2cda6909d0b32ad3371fe3
Filin15488 <filya13579@yandex.ru>
2024-02-16 11:02:19 +0300

    start rewrite header

--- www/login_verify_data.php
+++ www/login_verify_data.php
@@ -1,12 +1,24 @@
 <?php
+session_start();
+
 // echo "<pre>";
 // print_r($_POST);
 // echo "</pre>";
 
-require_once("./inclusion/db.php");
-$sql = "SELECT * FROM users WHERE name_user = \"" . $_POST["user_name"] . "\";";
-$stmt = sqlsrv_query ($conn, $sql);
+// require_once("./inclusion/db.php");
+// $sql = "SELECT * FROM users WHERE name_user = \"" . $_POST["user_name"] . "\";";
+// $stmt = sqlsrv_query ($conn, $sql);
 
-echo "<pre>";
-print_r($stmt);
-echo "</pre>";
+// echo "<pre>";
+// print_r($stmt);
+// echo "</pre>";
+
+require_once("./inclusion/db.php");
+
+$user_name = trim($_POST['user_name']);
+$user_pass = $_POST['user_pass'];
+$sql="SELECT [stego_as].[dbo].[role].[name_role] FROM [stego_as].[dbo].[users] INNER JOIN [stego_as].[dbo].[role] ON ([stego_as].[dbo].[users].[id_role]=[stego_as].[dbo].[role].[id_role]) WHERE [stego_as].[dbo].[users].[name_users] = '$user_pass'";
+$stmt = sqlsrv_query($conn, $sql); //Выполнение SQL-запроса
+if ($stmt == false){
+    header("Location: /login?act=login_err");
+}
