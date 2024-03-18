8a8ca8c2354b47c5ee34e29995e8f0c092d5df35
Filin15488 <filya13579@yandex.ru>
2024-03-18 19:06:49 +0300

    added script from insert stego in DB on stego add page

--- www/work_stego/stego_add.php
+++ www/work_stego/stego_add.php
@@ -18,7 +18,7 @@ require_once("../inclusion/security_admin.php");
         <div id="title_on_page">Добавить стенограмму</div>
         <div class="separator"></div>
         <div class="add_stego_form">
-            <form action="#" method="post">
+            <form action="./stego_add_insert_db.php" method="post">
                 <div class="add_stego_form_row">
                     <div class="add_stego_form_column-text">
                         Название стенограммы:
@@ -82,7 +82,7 @@ require_once("../inclusion/security_admin.php");
                         Система:
                     </div>
                     <div class="add_stego_form_column">
-                        <select name="stego_type" id="">
+                        <select name="stego_OS" id="">
                             <?php require_once("./type_OS_from_db.php"); ?>
                         </select>
                     </div>
@@ -92,16 +92,40 @@ require_once("../inclusion/security_admin.php");
                         Вид программы:
                     </div>
                     <div class="add_stego_form_column">
-                        <select name="stego_type" id="">
-                            <option value="1">Устанавливаемая</option>
-                            <option value="2">Портативная</option>
+                        <select name="stego_portable" id="">
+                            <option value="0">Устанавливаемая</option>
+                            <option value="1">Портативная</option>
                         </select>
                     </div>
                 </div>
                 <button type="submit" class="btn" style="margin: 24px;">
-                    Добавить пользователя
+                    ДОБАВИТЬ СТЕНОГРАММУ
                 </button>
-                
+                <?php
+                $error_list = [
+                    'err_stego_name_empty' => "Внимание! Поле с названием программы пустое",
+                    'err_stego_container_type_empty' => "Внимание! Поле с типом контейнера пустое",
+                    'err_stego_name_exist' => "Данная программа уже учтена",
+                    'err_insert' => 'Ошибка транзакции. Пожалуйста, попробуйте ещё'
+                ];
+                function error_div(string $type_error, string $message)
+                {
+                    if (!empty($_GET[$type_error])) {
+                        if ($_GET[$type_error] == '1') {
+                            $error_div = "<div class='login-errors'><h2>" . $message . "</h2></div>";
+                            echo $error_div;
+                        }
+                    };
+                };
+                if (!empty($_GET) && empty($_GET['insert_done'])) {
+                    error_div(array_key_first($_GET), $error_list[array_key_first($_GET)]);
+                } else {
+                    if (!empty($_GET) && $_GET['insert_done'] == 1) {
+                        echo "<div class='DONE-div'><h2>" . "Стенограмма была успешно добавлена" . "</h2></div>";
+                    }
+                }
+
+                ?>
 
 
 
