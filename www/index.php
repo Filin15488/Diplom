601b404d99d9ab3b00381abfa6732abcacbb10ea
Filin <filya13579@yandex.ru>
2024-06-11 23:17:34 +0300

    update accordion on index page

--- www/index.php
+++ www/index.php
@@ -8,6 +8,7 @@ require_once("./inclusion/security.php");
 
 <head>
     <?php require_once("./inclusion/head.php") ?>
+
 </head>
 
 <body>
@@ -22,77 +23,81 @@ require_once("./inclusion/security.php");
             <div class="main__container index__container">
                 <div class="titile_on_page">FAQ</div>
                 <div class="separator"></div>
-                <div class="faq">
-                    <div class="faq-item">
-                        <div class="faq-question">
-                            О проекте
-                        </div>
-                        <div class="faq-answer">
-                            "Stego_as" - web-сервис для поиска следов присутствия стеганографического программного обеспечения на компьютерах и носителях информации. Наша цель - повысить эффективность проведения компьютерно-технических экспертиз и исследований в рамках получения ориентирующей информации за счёт автоматизации.
-                            <br>
-                            Stego_as - это развивающийся продукт, присоединяйтесь к нам, чтобы сделать мир цифровой информации более прозрачным и защищённым!
-                        </div>
+
+
+                <div class="accordion__container">
+                    <div class="accordion">
+                        <dl>
+                            <dt>
+                                <a href="#accordion1" aria-expanded="false" aria-controls="accordion1" class="accordion-title accordionTitle js-accordionTrigger">О проекте</a>
+                            </dt>
+                            <dd class="accordion-content accordionItem is-collapsed" id="accordion1" aria-hidden="true">
+                                <p>
+                                    "Stego_as" - web-сервис для поиска следов присутствия стеганографического программного обеспечения на компьютерах и носителях информации. Наша цель - повысить эффективность проведения компьютерно-технических экспертиз и исследований в рамках получения ориентирующей информации за счёт автоматизации.
+                                    <br>
+                                    Stego_as - это развивающийся продукт, присоединяйтесь к нам, чтобы сделать мир цифровой информации более прозрачным и защищённым!
+                                </p>
+                            </dd>
+                            <dt>
+                                <a href="#accordion2" aria-expanded="false" aria-controls="accordion2" class="accordion-title accordionTitle js-accordionTrigger">
+                                    Принцип работы
+                                </a>
+                            </dt>
+                            <dd class="accordion-content accordionItem is-collapsed" id="accordion2" aria-hidden="true">
+                                <p>
+                                    Stego_as ведет учет хэш-значений файлов, записей в реестр и журналы операционных систем данных известных стеганографических программ для дальнейшего их сравнения их с объектами исследования
+                                </p>
+                            </dd>
+                            <dt>
+                                <a href="#accordion3" aria-expanded="false" aria-controls="accordion3" class="accordion-title accordionTitle js-accordionTrigger">
+                                    Преимущества Stego_as
+                                </a>
+                            </dt>
+                            <dd class="accordion-content accordionItem is-collapsed" id="accordion3" aria-hidden="true">
+
+                                <ol>
+                                    <li>
+                                        Автоматизация процесса анализа: исключает необходимость ручного анализа, экономя время и ресурсы экспертов.
+                                    </li>
+                                    <li>
+                                        Высокая точность: использование проверенных алгоритмов обеспечивает надёжность результатов.
+                                    </li>
+                                    <li>
+                                        Удобство использования: простой и понятный интерфейс делает работу с приложением доступной даже для пользователей без специальной технической подготовки.
+                                    </li>
+                                    <li>
+                                        Своевременные обновления: постоянное обновление базы данных на основе мониторинга ресурсов Интернет
+                                    </li>
+                                </ol>
+
+                            </dd>
+                            <dt>
+                                <a href="#accordion4" aria-expanded="false" aria-controls="accordion3" class="accordion-title accordionTitle js-accordionTrigger">
+                                    Кто может воспользоваться Stego_as?
+                                </a>
+                            </dt>
+                            <dd class="accordion-content accordionItem is-collapsed" id="accordion4" aria-hidden="true">
+
+                                <ol>
+                                    <li>
+                                        Эксперты компьютерно-технического направления: для повышения эффективности проведения экспертиз.
+                                    </li>
+                                    <li>
+                                        Органы правопорядка: для получения ориентирующей информации.
+                                    </li>
+                                    <li>
+                                        Корпоративные специалисты: при расследовании инцидентов ИБ.
+                                    </li>
+                                    <li>
+                                        Исследователи и студенты: для обучения и проведения научных исследований в области стеганоанализа.
+                                    </li>
+                                </ol>
+                            </dd>
+                        </dl>
                     </div>
-                    <div class="faq-item">
-                        <div class="faq-question">
-                            Принцип работы
-                        </div>
-                        <div class="faq-answer">
-                            Stego_as ведет учет хэш-значений файлов, записей в реестр и журналы операционных систем данных известных стеганографических программ для дальнейшего их сравнения их с объектами исследования
-                        </div>
-
-                    </div>
-                    <div class="faq-item">
-                        <div class="faq-question">
-                            Преимущества Stego_as
-                        </div>
-                        <div class="faq-answer">
-                            <ol>
-                                <li>
-                                    Эксперты компьютерно-технического направления: для повышения эффективности проведения экспертиз.
-                                </li>
-                                <li>
-                                    Органы правопорядка: для получения ориентирующей информации.
-                                </li>
-                                <li>
-                                Корпоративные специалисты: при расследовании инцидентов ИБ.
-                                </li>
-                                <li>
-                                    Исследователи и студенты: для обучения и проведения научных исследований в области стеганоанализа.
-                                </li>
-
-                            </ol>
-
-                        </div>
-                    </div>
-
-                    <div class="faq-item">
-                        <div class="faq-question">
-                            Кто может воспользоваться Stego_as?
-                        </div>
-                        <div class="faq-answer">
-                            <ol>
-                                <li>
-                                    Компьютерные эксперты: для повышения эффективности проведения экспертиз.
-                                </li>
-                                <li>
-                                    Органы правопорядка: для расследования преступлений, связанных со скрытием информации.
-                                </li>
-                                <li>
-                                    Корпоративные пользователи: для защиты корпоративных данных от потенциальных угроз.
-                                </li>
-                                <li>
-                                    Исследователи и студенты: для обучения и проведения научных исследований в области стеганоанализа.
-                                </li>
-                            </ol>
-
-                        </div>
-                    </div>
-
-
-
-                    <!-- Add more FAQ items as needed -->
                 </div>
+
+
 
             </div>
         </main>
--- www/js/index.js
+++ www/js/index.js
@@ -1,34 +1,65 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const faqItems = document.querySelectorAll('.faq-item');
 
-    faqItems.forEach(item => {
-        const question = item.querySelector('.faq-question');
 
-        question.addEventListener('click', () => {
-            const answer = item.querySelector('.faq-answer');
-            const isActive = item.classList.contains('active');
-
-            // Устанавливаем высоту контента, чтобы вычислить высоту для анимации
-            if (!isActive) {
-                item.classList.add('active');
-                answer.style.maxHeight = answer.scrollHeight + 'px';
-
-                // После анимации установим maxHeight в none, чтобы сохранить высоту
-                answer.addEventListener('transitionend', function handler() {
-                    answer.style.maxHeight = 'none';
-                    answer.removeEventListener('transitionend', handler);
-                });
-            } else {
-                // При скрытии блока сначала убираем maxHeight
-                answer.style.maxHeight = answer.scrollHeight + 'px';
-
-                // Для анимации нужно обернуть в setTimeout, чтобы браузер применил предыдущее изменение
-                setTimeout(() => {
-                    answer.style.maxHeight = '0px';
-                }, 0);
-
-                item.classList.remove('active');
-            }
-        });
-    });
-});
+//uses classList, setAttribute, and querySelectorAll
+//if you want this to work in IE8/9 youll need to polyfill these
+(function(){
+    var d = document,
+    accordionToggles = d.querySelectorAll('.js-accordionTrigger'),
+    setAria,
+    setAccordionAria,
+    switchAccordion,
+    touchSupported = ('ontouchstart' in window),
+    pointerSupported = ('pointerdown' in window);
+    
+    skipClickDelay = function(e){
+      e.preventDefault();
+      e.target.click();
+    }
+  
+      setAriaAttr = function(el, ariaType, newProperty){
+      el.setAttribute(ariaType, newProperty);
+    };
+    setAccordionAria = function(el1, el2, expanded){
+      switch(expanded) {
+        case "true":
+          setAriaAttr(el1, 'aria-expanded', 'true');
+          setAriaAttr(el2, 'aria-hidden', 'false');
+          break;
+        case "false":
+          setAriaAttr(el1, 'aria-expanded', 'false');
+          setAriaAttr(el2, 'aria-hidden', 'true');
+          break;
+        default:
+          break;
+      }
+    };
+  //function
+  switchAccordion = function(e) {
+    console.log("triggered");
+    e.preventDefault();
+    var thisAnswer = e.target.parentNode.nextElementSibling;
+    var thisQuestion = e.target;
+    if(thisAnswer.classList.contains('is-collapsed')) {
+      setAccordionAria(thisQuestion, thisAnswer, 'true');
+    } else {
+      setAccordionAria(thisQuestion, thisAnswer, 'false');
+    }
+      thisQuestion.classList.toggle('is-collapsed');
+      thisQuestion.classList.toggle('is-expanded');
+      thisAnswer.classList.toggle('is-collapsed');
+      thisAnswer.classList.toggle('is-expanded');
+    
+      thisAnswer.classList.toggle('animateIn');
+    };
+    for (var i=0,len=accordionToggles.length; i<len; i++) {
+      if(touchSupported) {
+        accordionToggles[i].addEventListener('touchstart', skipClickDelay, false);
+      }
+      if(pointerSupported){
+        accordionToggles[i].addEventListener('pointerdown', skipClickDelay, false);
+      }
+      accordionToggles[i].addEventListener('click', switchAccordion, false);
+    }
+  })();
+  
+  
