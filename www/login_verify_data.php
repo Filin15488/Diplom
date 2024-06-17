789579e57e350f1ddf73ba4bd40719e7cd18b871
Filin15488 <filya13579@yandex.ru>
2024-06-17 05:33:26 +0300

    release check on activate user

--- www/login_verify_data.php
+++ www/login_verify_data.php
@@ -8,48 +8,55 @@ $data = [
     "user_pass" => $_POST['user_pass']
 ];
 
-$err = array ();
+$err = array();
 
-$sql = "SELECT stego_as.dbo.users.[id_users], [stego_as].[dbo].[role].[name_role], stego_as.dbo.users.[password]
+$sql = "SELECT stego_as.dbo.users.[id_users], stego_as.[dbo].users.activate, [stego_as].[dbo].[role].[name_role], stego_as.dbo.users.[password]
 FROM [stego_as].[dbo].[users] INNER JOIN 
 [stego_as].[dbo].[role] ON 
 ([stego_as].[dbo].[users].[id_role]=[stego_as].[dbo].[role].[id_role]) 
 WHERE [stego_as].[dbo].[users].[name_users] = '" . $data['user_name'] . "'";
 
 $result = sqlsrv_query($conn, $sql);
-if ($result){
+if ($result) {
     // если запрос прошёл
-    if (sqlsrv_has_rows($result)){
-        while ($res = sqlsrv_fetch_array($result, SQLSRV_FETCH_ASSOC)){
+    if (sqlsrv_has_rows($result)) {
+        while ($res = sqlsrv_fetch_array($result, SQLSRV_FETCH_ASSOC)) {
             // нашёл строку с ролью и паролем
             $_SESSION['role'] = trim($res['name_role']);
             $pass_hash = $res['password'];
-            if (password_verify($data['user_pass'],$pass_hash)){
+            if (password_verify($data['user_pass'], $pass_hash)) {
                 // пароли совпадают
-                $_SESSION ['verify'] = true;
-                $_SESSION['id_users'] = $res['id_users'];
-                header("Location: ./index.php");
-            }
-            else {
+                if ($res['activate'] == 1) 
+                {
+                    // активирован
+                    $_SESSION['verify'] = true;
+                    $_SESSION['id_users'] = $res['id_users'];
+                    header("Location: ./index.php");
+                }
+                else
+                {
+                    // не активирован
+                    $err[] = 'activate_error';
+                    $url = url_redirect($err, 'login.php');
+                    header($url);
+                }
+            } else {
                 // пароли не совпадают
-                $err [] = 'pass_error';
+                $err[] = 'pass_error';
                 $url = url_redirect($err, 'login.php');
                 header($url);
-
             }
         }
-    }
-    else 
-    {
+    } else {
         // пользователя нет
         // echo "пользователя нет";
-        $err [] = "user_error";
-        $url = url_redirect($err,'login.php');
+        $err[] = "user_error";
+        $url = url_redirect($err, 'login.php');
         header($url);
     }
 }
 if ($result == false) {
-    $err [] = "query_err";
-    $url = url_redirect($err,'login.php');
+    $err[] = "query_err";
+    $url = url_redirect($err, 'login.php');
     header($url);
 }
